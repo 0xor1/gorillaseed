ee2683fd8a053b0b2260a2f06233b93c607006d4
Daniel Robinson <daniel.robinson.open@gmail.com>
2015-03-09 22:44:51 +0000

    add some dependencies, and load client side libs

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 gorillaseed
 ============
 
+##Prerequisits
+
+Install:
+
+* [Go](http://golang.org/)
+* [Node](https://nodejs.org/)
+
+Run:
+
+```
+> npm install
+```
+
--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,21 +1,37 @@
-$(document).ready(function(){
-
-    var counter = $('span#global-counter')
-
-    $('#refresh-button').click(getGlobalCounter);
-    $('#increment-button').click(incrementGlobalCounter);
-
-    function getGlobalCounter() {
-        $.post('api/v1/counter/get', success);
-    }
-
-    function incrementGlobalCounter(){
-        $.post('api/v1/counter/increment', success);
-    }
-
-    function success(data){
-        counter.text(data.counter);
+require.config({
+    baseUrl: 'js',
+    paths: {
+        '$': 'lib/jquery-2.1.3',
+        'ng': 'lib/angular-1.3.14'
     }
 });
 
+require([
+    '$',
+    'ng'
+],
+function(
+    $,
+    ng){
 
+    $(document).ready(function() {
+
+        var counter = $('span#global-counter')
+
+        $('#refresh-button').click(getGlobalCounter);
+        $('#increment-button').click(incrementGlobalCounter);
+
+        function getGlobalCounter() {
+            $.post('api/v1/counter/get', success);
+        }
+
+        function incrementGlobalCounter() {
+            $.post('api/v1/counter/increment', success);
+        }
+
+        function success(data) {
+            counter.text(data.counter);
+        }
+    });
+
+});

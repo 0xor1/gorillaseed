864feaf6e81adac750f2a1867369b84c75e03625
Daniel Robinson <daniel.robinson.open@gmail.com>
2015-03-19 20:24:28 +0000

    add more counter endpoint unit tests and add test results and code coverage to repo

--- src/server/src/api/v1/counter/counter_test.go
+++ src/server/src/api/v1/counter/counter_test.go
@@ -7,12 +7,18 @@ import(
 	"net/http/httptest"
 	"github.com/0xor1/gorillaseed/src/server/lib/assert"
 	"github.com/0xor1/gorillaseed/src/server/src/util"
+	"github.com/0xor1/gorillaseed/src/server/lib/sessions"
 )
 
 func Test__returnCounter(t *testing.T){
+	var c _counter
 	w := httptest.NewRecorder()
-	_returnCounter(w, 1)
+
+	_returnCounter(w, 123)
+
+	util.ReadJson(w, &c)
 	assert.Equal(t, strings.Contains(w.Body.String(), "value"), true, "'Value' should be encoded to 'value' in json")
+	assert.Equal(t, c.Value, 123, "Value should equal second parameter")
 }
 
 func Test_getGlobalCounter(t *testing.T){
@@ -24,6 +30,7 @@ func Test_getGlobalCounter(t *testing.T){
 	getGlobalCounter(w, req)
 
 	util.ReadJson(w, &c)
+	assert.Equal(t, globalCounter, 123, "globalCounter should be unchanged")
 	assert.Equal(t, c.Value, globalCounter, "Value should equal globalCounter")
 }
 
@@ -40,4 +47,26 @@ func Test_incrementGlobalCounter(t *testing.T){
 	assert.Equal(t, c.Value, globalCounter, "Value should equal globalCounter")
 }
 
-//TODO write myCounter tests
+func Test_getMyCounter(t *testing.T){
+	var c _counter
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "", nil)
+	sessionStore = sessions.NewCookieStore()
+
+	getMyCounter(w, req)
+
+	util.ReadJson(w, &c)
+	assert.Equal(t, c.Value, 0, "Value should be initialised to 0")
+}
+
+func Test_incrementMyCounter(t *testing.T){
+	var c _counter
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "", nil)
+	sessionStore = sessions.NewCookieStore()
+
+	incrementMyCounter(w, req)
+
+	util.ReadJson(w, &c)
+	assert.Equal(t, c.Value, 1, "Value should be initialised to 0 and then incremented to 1")
+}
